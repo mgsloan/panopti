26918c4c7feadb2159900bcf2c9fa485d8db7c51
Michael Sloan <mgsloan@lappy.(none)>
2011-09-03 15:29:00 -0700

    Remove cruft

--- Hackel.hs
+++ Hackel.hs
@@ -60,7 +60,6 @@ updateParse = lensed code parsed parseDecl
 sourceDir = "source"
 
 main = createDirectoryIfMissing False sourceDir
---    >> print "hi!"
     >> startGHCiServer [sourceDir] fail print
     >>= \chan -> runToy $ Toy
     { initialState = updateParse $ 
@@ -113,16 +112,10 @@ handleDisplay _ (tl, br) s@(State txt ix p c (_, ypos)) = do
   case p of
     ParseOk decl -> drawApps (textPos ^-^ (0, 20)) txt c
                              (concatApps $ getApps decl)
---                    drawSpans astPos txt (getSpans decl)
     f@(ParseFailed _ _) -> C.showText (show f)
   C.stroke
 
   return s
-
-drawLabeledLine :: String -> DLine -> C.Render ()
-drawLabeledLine txt lin = do
-  draw lin
-  relText 0.5 (lin `at` 0.5 ^-^ (0, 7)) txt
 
 reverseLinear :: Linear Double -> Linear Double
 reverseLinear = (`compose` (Linear 1 0 :: Linear Double))
@@ -131,29 +124,12 @@ spanLine :: String -> Ivl -> C.Render DLine
 spanLine txt (f, t) = liftM (mapT reverseLinear . rside 2 . expandR 2) 
                     $ textRect txt (f - 1) (t - 1)
 
-drawSpans :: DPoint  -> String -> [(Ivl, String)] -> C.Render ()
-drawSpans pos txt =
-      -- Draw each labeled line, with each subsequent line 15 pixels lower.
-  (>>= zipWithM_ (\d (l, n) -> drawLabeledLine n
-                             $ offset (pos ^+^ (0, 15) ^* fromIntegral d) l)
-                 [0..])
-
-      -- Turn each span into an appropriately sized line segment.
-  . mapM (\(s, n) -> liftM (, n) $ spanLine txt s)
-
-      -- Prefer last of all identically-spanned tokens.  Pretty arbitrary.
-  . map last . groupBy ((==) `on` (\(x,_)->x))
-
 srcSpan :: SrcSpanInfo -> Ivl
 srcSpan = (srcSpanStartColumn &&& srcSpanEndColumn) .  srcInfoSpan
 
 getSpan :: (Data a) => a -> Maybe Ivl
 getSpan = listToMaybe . catMaybes
         . gmapQ (const Nothing `extQ` (Just . srcSpan))
-
-getSpans :: (Data a) => a -> [(Ivl, String)]
-getSpans x = maybeToList (fmap (, show $ toConstr x) $ getSpan x)
-          ++ concat (gmapQ getSpans x)
 
 getSpan' :: (Data a) => a -> Ivl
 getSpan' = fromJust . getSpan
@@ -166,7 +142,6 @@ type Apps = [(Ivl, [Ivl])]
 getApps :: forall a. (Data a) => a -> Apps
 getApps ast = ((const []) `extQ` processExp) ast ++ recurse ast
  where 
-  --processExp (InfixApp l (QVarOp ()) = (getSpan l, [getSpan r])
   processExp :: Exp SrcSpanInfo -> Apps
   processExp (InfixApp _ l o r) = case o of
     (QVarOp _ (UnQual _ (Symbol _ "."))) -> [(getSpan' l, [getSpan' r])]
@@ -182,14 +157,7 @@ concatApps = map (second $ reverse . concat)
            . combineFstOn (\((x, _), _) -> x)
            . reverse
 
-
-{-
-addTypes :: String
-         -> [(Int, Int)]
-         -> IO [((Int, Int), Maybe String)]
-addTypes code = mapM (\x -> liftM (x,) $ getType x)
- where  -}-- takes an IO function and returns a cached version
-
+-- takes an IO function and returns a cached version
 memoIO f = do
   v <- newMVar M.empty
   let f' x = do
@@ -202,15 +170,6 @@ memoIO f = do
           Just r  -> return r
   return f'
 
-{-memoCatch f = unsafePerformIO $ memoIO $ \x -> 
---  (liftM Just $ f x) --`Simple.catchError_fixed` (const $ return Nothing)
-
-getType' = memoCatch I.typeOf
-
-getType :: (Int, Int) -> String -> IO (Maybe String)
-getType (f, t) = getType' . 
--}
-
 -- Sorts and groups based on some derived, comparable property
 groupSortOn f = groupBy ((==) `on` f) . sortBy (comparing f)
 
@@ -221,50 +180,104 @@ substr (f, t) = take (t - f) . drop (f - 1)
 
 eitherToMaybe = either (const Nothing) Just
 
--- | Conditionally run an action, using a @Maybe a@ to decide.
-whenJust :: Monad m => Maybe a -> (a -> m ()) -> m ()
-whenJust mg f = maybe (return ()) f mg
-
-exprName (f, t) = "__" ++ show f ++ "_" ++ show t
-
-subExpr :: String -> Ivl -> String
-subExpr full r = "(" ++ substr r full ++ ")"
-
-writeExprs :: String -> [Ivl] -> IO String
-writeExprs full exprs = do
-    exists <- doesFileExist filepath
-    when (not exists) $ writeFile filepath code2
-    return filename
-  where filename = "M" ++ show (abs $ hash code)
-        filepath = sourceDir </> filename <.> "hs"
-        code = full ++ "\n"
-            ++ concatMap (\r -> exprName r ++ " = "
-                             ++ subExpr full r ++ "\n") exprs
-        code2 = "module " ++ filename ++ " where\n" ++ code
-
-{-
-    mapm_ (\(s, xs) -> lifta2 
-      (const $ (c.liftio . print :: [(linear double, linear double)] -> c.render ()))
-      (spanline txt s) $ mapm (spanline txt) xs)
- -}
+isRight (Right _) = True
+isRight _ = False
 
 type TypeMap = M.Map Ivl (Type SrcSpanInfo)
 type ColorMap = M.Map (Type SrcSpanInfo) (Int, Int, Int)
 type HeightMap = IM.IntervalMap Int Double
 
-isRight (Right _) = True
-isRight _ = False
+getTypeMap :: String -> TaskChan -> Apps -> IO TypeMap
+getTypeMap txt c apps = do
+  rec <- liftM isRight . getType . recText $ words txt !! 0
+  fs <- mapM (getExpr rec . fst) apps
+  ps <- mapM (mapM (getExpr rec) . snd) apps
+  return . M.fromList . map (second cannonicalType) . catMaybes $ fs ++ concat ps
+ where
+  getExpr :: Bool -> Ivl -> IO (Maybe (Ivl, Type SrcSpanInfo))
+  getExpr rec ivl
+    = liftM (\t -> eitherToMaybe t >>=
+        liftM (ivl,) . eitherToMaybe . parseResultToEither . parseType)
+    . getType . (if rec then recText else id) $ "(" ++ substr ivl txt ++ ")"
+  getType = interpret c "MyMain" . I.typeOf
+  recText x = "let {" ++ txt ++ "} in " ++ x
 
-ivlHull (f1, t1) (f2, t2) = (min f1 f2, max t1 t2)
+getHeight :: (Double, Double) -> Ivl -> IM.IntervalMap Int Double -> Double
+getHeight (_, y) (f, t) = minimum . (y:) . map snd . intersections (IM.Interval f t)
+
+drawApps :: (Double, Double) -> String -> TaskChan -> Apps -> C.Render ()
+drawApps pos txt c apps = do
+    tm <- C.liftIO $ getTypeMap txt c apps
+    let tc = M.fromList . (`zip` colours) . nub . sort
+           . catMaybes . map (`M.lookup` tm) $ concatMap snd apps
+    drawLegend tc
+    C.setLineWidth 1
+    foldM_ (drawFunc tm tc) IM.empty $ reverse apps
+ where
+  drawLegend :: ColorMap -> C.Render ()
+  drawLegend = zipWithM_ (\y (t, c) -> do
+      setColor c
+      relText 0 (fst pos, y) (prettyPrint t))
+    [220, 240..] . M.toList
+  drawFunc :: TypeMap -> ColorMap -> HeightMap -> (Ivl, [Ivl]) -> C.Render HeightMap
+  drawFunc tm tc hm (func, params) = do
+    fspan <- spanLine txt func
+    foldM (drawParam fspan) hm 
+        . catMaybes
+        . map (\r -> liftM (func, r,) $ M.lookup r tm)
+        $ reverse params
+   where
+    drawParam :: DLine -> HeightMap -> (Ivl, Ivl, Type SrcSpanInfo) -> C.Render HeightMap
+    drawParam fs hm (fivl, pivl, t) = do
+      ps <- spanLine txt pivl
+      let height = getHeight pos (snd fivl, snd pivl) hm
+          fs' = offset (fst pos, height) fs
+          ps' = offset (fst pos, height) ps
+          f = fs' `at` 1 ^-^ (5, 0)
+          pl = ps' `at` 0 ^+^ (4, 0)
+          pm = ps' `at` 0.5
+          pr = ps' `at` 1 ^+^ (-4, 0)
+     C.liftIO $ print (fivl, pivl, height)
+      maybe (setColor ((0,0,0) :: DColor)) setColor (M.lookup t tc)
+      move $ f ^+^ (3,0)
+      C.arc (fst f) (snd f) 3 0 (2 * pi)
+      C.fill
+      if ' ' `elem` substr pivl txt then do
+        draw $ bezierFromPoints [f, pr]
+        draw $ bezierFromPoints [f, pl]
+        draw $ bezierFromPoints [pl, pl ^+^ (0, 4)]
+        draw $ bezierFromPoints [pr, pr ^+^ (0, 4)]
+       else do
+        draw $ bezierFromPoints [f, pm]
+      C.stroke
+      return $ IM.insert (uncurry IM.Interval $ pivl) (height - 10) hm
+
+colours :: [(Int, Int, Int)]
+colours = cycle . map (\c -> (fromIntegral $ channelRed c, 
+                              fromIntegral $ channelGreen c,
+                              fromIntegral $ channelBlue c))
+                $ map toSRGB24
+  [ Color.lightgreen
+  , Color.steelblue
+  , Color.yellowgreen
+  , Color.peru
+  , Color.gold
+  , Color.cyan
+  , Color.lightsalmon
+  ]
+
+-- | Conditionally run an action, using a @Maybe a@ to decide.
+whenJust :: Monad m => Maybe a -> (a -> m ()) -> m ()
+whenJust mg f = maybe (return ()) f mg
+
+type STT = ST.State (String, M.Map String Char)
 
 floatFst (f, s) = pure (,s) <*> f
 floatSnd (f, s) = pure (f,) <*> s
 floatBoth (f, s)  = liftA2 (,) (pure f) (pure s)
 
-type STT = ST.State (String, M.Map String Char)
-
-cannonicalType2 :: Type SrcSpanInfo -> Type SrcSpanInfo
-cannonicalType2 t = ST.evalState (rec t) (['a'..], M.empty)
+cannonicalType :: Type SrcSpanInfo -> Type SrcSpanInfo
+cannonicalType t = ST.evalState (rec t) (['a'..], M.empty)
  where
   doVar :: String -> STT String
   doVar other = do
@@ -301,167 +314,3 @@ cannonicalType2 t = ST.evalState (rec t) (['a'..], M.empty)
    `extM` (\n -> doName n >>= return . snd)
    `extM` preserveQ
    `extM` doType
-
-{-
-cannonicalType :: Type l -> Type l
-cannonicalType t = ST.evalState (rec t) (['a'..], M.empty)
- where
-  doVar other = do
-    (xs, m) <- ST.get
-    case M.lookup other m of
-      Just x -> return [x]
-      Nothing -> ST.put (tail xs, M.insert other (head xs) m)
-              >> return [head xs]
-
-  removeVar other = ST.modify (second $ M.delete other)
-
-  doName (Ident l n)  = doVar n >>= \n' -> return (n, Ident l n')
-  doName (Symbol l n) = doVar n >>= \n' -> return (n, Symbol l n')
-
-  doBind (KindedVar l n k) = do
-    (o, n') <- doName n
-    return (o, KindedVar l n' k)
-
---  rec :: Type l -> ST.State (String, M.Map String Char) (Type l)
-  rec (TyForall l bnds ctx t) = do
-    bs <- maybe (return Nothing) (liftM Just . mapM doBind) bnds
-    cx <- maybe (return Nothing) (liftM Just . recCx) ctx
-    t' <- rec t
-    whenJust bs $ mapM_ (removeVar . fst)
-    return $ TyForall l (liftM (map snd) bs) cx t'
-
-  rec (TyFun l a b) = do
-    a' <- rec a
-    b' <- rec b
-    return $ TyFun l a' b'
-
-  rec (TyApp l a b) = do
-    a' <- rec a
-    b' <- rec b
-    return $ TyApp l a' b'
-
-  rec (TyVar l n) = doName n >>= return . TyVar l . snd
-
-  rec (TyTuple l b ts) = mapM rec ts >>= return . TyTuple l b
-  rec (TyList l t)   = rec t >>= return . TyList l
-  rec (TyParen l t)  = rec t >>= return . TyParen l
-  rec (TyKind l t k) = rec t >>= return . (flip (TyKind l) $ k)
-
-  rec x = return x
-  --TODO: make sure?
-  --rec (TyCon l n) = 
-  --rec (TyInfix l t n t') = 
-
-  recCx (CxSingle l a) = recA a       >>= return . CxSingle l
-  recCx (CxTuple l as) = mapM recA as >>= return . CxTuple l
-  recCx (CxParen l c)  = recCx c      >>= return . CxParen l
-  recCx x = return x -- CxEmpty
-
-  recA (ClassA l n ts)  = mapM rec ts >>= return . ClassA l n
-  recA (InfixA l a n b) = do
-    a' <- rec a
-    b' <- rec b
-    return $ InfixA l a' n b'
-  recA (IParam l p t) = rec t >>= return . IParam l p
-  recA (EqualP l a b) = do
-    a' <- rec a
-    b' <- rec b
-    return $ EqualP l a' b'
--}
-
-getTypeMap :: String -> TaskChan -> Apps -> IO TypeMap
-getTypeMap txt c apps = do
-  rec <- liftM isRight . getType . recText $ words txt !! 0
-  fs <- mapM (getExpr rec . fst) apps
-  ps <- mapM (mapM (getExpr rec) . snd) apps
-  return . M.fromList . map (second cannonicalType2) . catMaybes $ fs ++ concat ps
- where
-  getExpr :: Bool -> Ivl -> IO (Maybe (Ivl, Type SrcSpanInfo))
-  getExpr rec ivl
-    = liftM (\t -> eitherToMaybe t >>=
-        liftM (ivl,) . eitherToMaybe . parseResultToEither . parseType)
-    . getType . (if rec then recText else id) $ subExpr txt ivl
-  getType = interpret c "MyMain" . I.typeOf
-  recText x = "let {" ++ txt ++ "} in " ++ x
-
-getHeight :: (Double, Double) -> Ivl -> IM.IntervalMap Int Double -> Double
-getHeight (_, y) (f, t) = minimum . (y:) . map snd . intersections (IM.Interval f t)
-
-drawApps :: (Double, Double) -> String -> TaskChan -> Apps -> C.Render ()
-drawApps pos txt c apps = do
-    tm <- C.liftIO $ getTypeMap txt c apps
-    let tc = M.fromList . (`zip` colours) . nub . sort
-           . catMaybes . map (`M.lookup` tm) $ concatMap snd apps
-    drawLegend tc
---    liftIO $ print tc
-    liftIO $ print tm
---    liftIO $ print . map (first (`substr` txt)) $ M.toList tm
---    liftIO . print $ map (first (`substr` txt)) apps
-    C.setLineWidth 1
-    foldM_ (drawFunc tm tc) IM.empty $ reverse apps
- where
-  drawLegend :: ColorMap -> C.Render ()
-  drawLegend = zipWithM_ (\y (t, c) -> do
-      setColor c
-      relText 0 (fst pos, y) (prettyPrint t))
-    [220, 240..] . M.toList
-  drawFunc :: TypeMap -> ColorMap -> HeightMap -> (Ivl, [Ivl]) -> C.Render HeightMap
-  drawFunc tm tc hm (func, params) = do
-    fspan <- spanLine txt func
-    foldM (drawParam fspan) hm 
-        . catMaybes
-        . map (\r -> liftM (func, r,) $ M.lookup r tm)
-        $ reverse params
-   where
-    drawParam :: DLine -> HeightMap -> (Ivl, Ivl, Type SrcSpanInfo) -> C.Render HeightMap
-    drawParam fs hm (fivl, pivl, t) = do
-      ps <- spanLine txt pivl
-      let height = getHeight pos (snd fivl, snd pivl) hm
-          fs' = offset (fst pos, height) fs
-          ps' = offset (fst pos, height) ps
-          f = fs' `at` 1 ^-^ (5, 0)
-          pl = ps' `at` 0 ^+^ (4, 0)
-          pm = ps' `at` 0.5
-          pr = ps' `at` 1 ^+^ (-4, 0)
---        l = magnitude (f ^-^ p)
-{-        bez = bezierFromPoints
-              [ f ^+^ (1, 0)
-              , f ^+^ (1, -l * 0.5)
-              , p ^+^ ((if p < f then l else -l) * 0.5, 0)
-              , p] -}
-      C.liftIO $ print (fivl, pivl, height)
-      maybe (setColor ((0,0,0) :: DColor)) setColor (M.lookup t tc)
-      move $ f ^+^ (3,0)
-      C.arc (fst f) (snd f) 3 0 (2 * pi)
-      C.fill
-      if ' ' `elem` substr pivl txt then do
-        draw $ bezierFromPoints [f, pr]
-        draw $ bezierFromPoints [f, pl]
-        draw $ bezierFromPoints [pl, pl ^+^ (0, 4)]
-        draw $ bezierFromPoints [pr, pr ^+^ (0, 4)]
-       else do
-        draw $ bezierFromPoints [f, pm]
-      C.stroke
-      return $ IM.insert (uncurry IM.Interval $ pivl) (height - 10) hm
---      relText (0.5, 0.5) ((bez `at` 1) ^-^ (0, 10)) t
-
-    {- draw . moveR (0, 10 * d) . expandR (2.0, 1.0) $
-       singletonR (fs `at` (d * step) ^+^ pos)
-
-
-
-     -}
-
-colours :: [(Int, Int, Int)]
-colours = cycle . map (\c -> (fromIntegral $ channelRed c, 
-                              fromIntegral $ channelGreen c,
-                              fromIntegral $ channelBlue c))
-                $ map toSRGB24
-  [ Color.lightgreen
-  , Color.steelblue
-  , Color.yellowgreen
-  , Color.peru
-  , Color.gold
-  , Color.cyan
-  , Color.lightsalmon
-  ]
